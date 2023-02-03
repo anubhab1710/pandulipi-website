9068b4d2f306af5e199e45ff0b3b82b779bcb92b
AtriSukul1508 <95478617+AtriSukul1508@users.noreply.github.com>
2023-02-03 11:11:39 +0530

    added team page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import About from "./components/About";
 import ContactUs from "./components/ContactUs";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import Team from "./components/Team/Team";
 
 function App() {
   return (
     <div className="font-oranienbaum">
-      <Navbar/>
-      <About/>
-      <ContactUs/>
-      <Footer/>
+      <Navbar />
+      <Team />
+      <About />
+      <ContactUs />
+      <Footer />
     </div>
   );
 }
