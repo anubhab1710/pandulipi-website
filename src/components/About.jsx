34fb38ba76e5da2b7f4225c2a82b83d64c834a9e
Arjun Choudhury <arjunchoudhury11202@gmail.com>
2023-02-03 13:13:32 +0530

    Made About page responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,25 +3,25 @@ import React from 'react'
 function About() {
   return (
     <>
-    <div className='bg-[#EEEEEE] h-[720px] w-full'>
+    <div className='bg-[#EEEEEE]  w-full'>
         <h1 className='text-center text-5xl p-10 '>About Us</h1>
-        <div className='flex justify-center items-center'>
-            <div className='text-xl max-w-4xl text-center m-10 lg:text-right'>
+        <div className='flex flex-col justify-center mb-10 items-center xl:flex-row-reverse'>
+            <img src="https://3.bp.blogspot.com/-1IG_dpBvLqQ/Ta_sTsPwFgI/AAAAAAAAACk/ga8pkFXy2WU/s1600/free+business+powerpoint+templates.jpg" alt="" className='m-10 p-10 h-[400px]'/>
+            <div className='text-xl max-w-4xl text-center m-10 xl:text-right'>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad recusandae adipisci mollitia, deserunt quaerat repellat expedita nemo consectetur pariatur nulla odit minima sint asperiores suscipit voluptates deleniti provident, dicta facilis.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad recusandae adipisci mollitia, deserunt quaerat repellat expedita nemo consectetur pariatur nulla odit minima sint asperiores suscipit voluptates deleniti provident, dicta facilis.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad recusandae adipisci mollitia, deserunt quaerat repellat expedita nemo consectetur pariatur nulla odit minima sint asperiores suscipit voluptates deleniti provident, dicta facilis
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad recusandae adipisci mollitia, deserunt quaerat repellat expedita nemo consectetur pariatur nulla odit minima sint asperiores suscipit voluptates deleniti provident, dicta facilisLorem ipsum dolor sit, amet consectetur adipisicing elit. Ad recusandae adipisci mollitia, deserunt quaerat repellat expedita nemo consectetur pariatur nulla odit minima sint asperiores suscipit voluptates deleniti provident, dicta facilis
                 </p>
                 <br />
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad recusandae adipisci mollitia, deserunt quaerat repellat expedita nemo consectetur pariatur nulla odit minima sint asperiores suscipit voluptates deleniti provident, dicta facilis.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad recusandae adipisci mollitia, deserunt quaerat repellat expedita nemo consectetur pariatur nulla odit minima sint asperiores suscipit voluptates deleniti provident, dicta facilis</p>
             </div>
-            <img src="https://3.bp.blogspot.com/-1IG_dpBvLqQ/Ta_sTsPwFgI/AAAAAAAAACk/ga8pkFXy2WU/s1600/free+business+powerpoint+templates.jpg" alt="" className='h-1 m-10 lg:h-[400px]'/>
         </div>
     </div>
-    <div className='bg-[#252B42] h-[720px] text-white'>
+    <div className='bg-[#252B42] text-white'>
         <h1 className='text-center text-5xl p-10'>From the Founder's Desk</h1>
         <div className='flex flex-col items-center'>
-        <div className='flex justify-center items-center'>
-            <img src="https://www.navidkp.com/wp-content/uploads/2020/12/The-Almanack-of-Naval-Ravikant-Cover-Image-200x300.png" alt="" className='h-[310px] m-10'/>
-            <div className='max-w-4xl text-xl text-left m-10'>
+        <div className='flex flex-col justify-center items-center xl:flex-row'>
+            <img src="https://www.navidkp.com/wp-content/uploads/2020/12/The-Almanack-of-Naval-Ravikant-Cover-Image-200x300.png" alt="" className='visible xl:visible lg:h-[310px] m-10'/>
+            <div className='max-w-4xl text-xl text-center m-10 xl:text-left'>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad recusandae adipisci mollitia, deserunt quaerat repellat expedita nemo consectetur pariatur nulla odit minima sint asperiores suscipit voluptates deleniti provident, dicta facilis.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad recusandae adipisci mollitia, deserunt quaerat repellat expedita nemo consectetur pariatur nulla odit minima sint asperiores suscipit voluptates deleniti provident, dicta facilis.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad recusandae adipisci 
                
                 </p>
@@ -29,15 +29,15 @@ function About() {
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad recusandae adipisci mollitia, deserunt quaerat repellat expedita nemo consectetur pariatur nulla odit minima sint asperiores suscipit voluptates deleniti provident, dicta facilis.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad recusandae adipisci mollitia, deserunt quaerat repellat expedita nemo consectetur pariatur nulla odit minima sint asperiores suscipit voluptates deleniti provident, dicta facilis</p>
             </div>
         </div>
-        <div className=''>
-            <p className='text-xl text-left max-w-7xl'>
+        <div className='p-10'>
+            <p className='lg:text-left text-xl text-center max-w-7xl '>
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae est et voluptate nulla nam reprehenderit, quaerat ratione illo cupiditate hic officiis minima? Facilis tenetur ducimus accusamus eos laborum a sint.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae est et voluptate nulla nam reprehenderit, quaerat ratione illo cupiditate hic officiis minima? Facilis tenetur ducimus accusamus eos laborum a sint.
             </p>
             <br />
-            <p className='text-xl text-left max-w-7xl font-semibold'>
+            <p className='text-xl text-center max-w-7xl font-semibold lg:text-left'>
                 Best,
             </p>
-            <p className='text-xl text-left max-w-7xl font-semibold'>
+            <p className='text-xl text-center max-w-7xl font-semibold lg:text-left'>
                 Lorem Ispum
             </p>
         </div>
