505e2bd630d110a2127ec76a18d4060b2f059806
Arjun Choudhury <arjunchoudhury11202@gmail.com>
2023-02-05 23:10:09 +0530

    Added Latest Events section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Hero from './Hero'
 import Hero2 from './Hero2'
+import Hero3 from './Hero3'
 import Hero4 from './Hero4'
 
 function Home() {
@@ -8,6 +9,7 @@ function Home() {
     <>
         <Hero/>
         <Hero2/>
+        <Hero3/>
         <Hero4/>
     </>
   )
