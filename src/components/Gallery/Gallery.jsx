e9728f83565e2d226d24d889a6e34f08fadf9184
Sayan Kumar Bhowmick <sayankr007@gmail.com>
2023-02-08 00:10:56 +0530

    Albums and Load More/Less Feature Added

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,242 +1,33 @@
 import React, { useState } from 'react'
-import Modal from './Modal';
+import { Routes, NavLink, Route } from "react-router-dom";
+import Album1 from './Album1';
+import Album2 from './Album2';
+import Album3 from './Album3';
+// import Modal from './Modal';
 import './style.css';
 
+
 const Gallery = () => {
-    const column1 = [
-        {
-            "name": "Lorem Ipsum",
-            "description": "Lorem Ipsum Lorem",
-            "imgLink": "https://images.pexels.com/photos/1770809/pexels-photo-1770809.jpeg"
-        },
-        {
-            "name": "Lorem Ipsum",
-            "description": "Lorem Ipsum Lorem",
-            "imgLink": "https://images.pexels.com/photos/4101555/pexels-photo-4101555.jpeg"
-        },
-        {
-            "name": "Lorem Ipsum",
-            "description": "Lorem Ipsum Lorem",
-            "imgLink": "https://images.pexels.com/photos/2113566/pexels-photo-2113566.jpeg"
-        },
-        {
-            "name": "Lorem Ipsum",
-            "description": "Lorem Ipsum Lorem",
-            "imgLink": "https://images.pexels.com/photos/1784577/pexels-photo-1784577.jpeg"
-        },
-        {
-            "name": "Lorem Ipsum",
-            "description": "Lorem Ipsum Lorem",
-            "imgLink": "https://images.pexels.com/photos/3493777/pexels-photo-3493777.jpeg"
-        },
-        {
-            "name": "Lorem Ipsum",
-            "description": "Lorem Ipsum Lorem",
-            "imgLink": "https://images.pexels.com/photos/15286/pexels-photo.jpg"
-        },
-        {
-            "name": "Lorem Ipsum",
-            "description": "Lorem Ipsum Lorem",
-            "imgLink": "https://images.pexels.com/photos/3244513/pexels-photo-3244513.jpeg"
-        },
-        {
-            "name": "Lorem Ipsum",
-            "description": "Lorem Ipsum Lorem",
-            "imgLink": "https://images.pexels.com/photos/3493777/pexels-photo-3493777.jpeg"
-        },
-    ]
-    const column2 = [
-        {
-            "name": "Lorem Ipsum",
-            "description": "Lorem Ipsum Lorem",
-            "imgLink": "https://images.pexels.com/photos/4101555/pexels-photo-4101555.jpeg"
-        },
-        {
-            "name": "Lorem Ipsum",
-            "description": "Lorem Ipsum Lorem",
-            "imgLink": "https://images.pexels.com/photos/3493777/pexels-photo-3493777.jpeg"
-        },
-        {
-            "name": "Lorem Ipsum",
-            "description": "Lorem Ipsum Lorem",
-            "imgLink": "https://images.pexels.com/photos/15286/pexels-photo.jpg"
-        },
-        {
-            "name": "Lorem Ipsum",
-            "description": "Lorem Ipsum Lorem",
-            "imgLink": "https://images.pexels.com/photos/2113566/pexels-photo-2113566.jpeg"
-        },
-        {
-            "name": "Lorem Ipsum",
-            "description": "Lorem Ipsum Lorem",
-            "imgLink": "https://images.pexels.com/photos/1784577/pexels-photo-1784577.jpeg"
-        },
-        {
-            "name": "Lorem Ipsum",
-            "description": "Lorem Ipsum Lorem",
-            "imgLink": "https://images.pexels.com/photos/1770809/pexels-photo-1770809.jpeg"
-        },
-        {
-            "name": "Lorem Ipsum",
-            "description": "Lorem Ipsum Lorem",
-            "imgLink": "https://images.pexels.com/photos/3244513/pexels-photo-3244513.jpeg"
-        },
-        {
-            "name": "Lorem Ipsum",
-            "description": "Lorem Ipsum Lorem",
-            "imgLink": "https://images.pexels.com/photos/3493777/pexels-photo-3493777.jpeg"
-        },
-    ]
-    const column3 = [
-        {
-            "name": "Lorem Ipsum",
-            "description": "Lorem Ipsum Lorem",
-            "imgLink": "https://images.pexels.com/photos/1784577/pexels-photo-1784577.jpeg"
-        },
-        {
-            "name": "Lorem Ipsum",
-            "description": "Lorem Ipsum Lorem",
-            "imgLink": "https://images.pexels.com/photos/4101555/pexels-photo-4101555.jpeg"
-        },
-        {
-            "name": "Lorem Ipsum",
-            "description": "Lorem Ipsum Lorem",
-            "imgLink": "https://images.pexels.com/photos/2113566/pexels-photo-2113566.jpeg"
-        },
-        {
-            "name": "Lorem Ipsum",
-            "description": "Lorem Ipsum Lorem",
-            "imgLink": "https://images.pexels.com/photos/3493777/pexels-photo-3493777.jpeg"
-        },
-        {
-            "name": "Lorem Ipsum",
-            "description": "Lorem Ipsum Lorem",
-            "imgLink": "https://images.pexels.com/photos/1770809/pexels-photo-1770809.jpeg"
-        },
-        {
-            "name": "Lorem Ipsum",
-            "description": "Lorem Ipsum Lorem",
-            "imgLink": "https://images.pexels.com/photos/15286/pexels-photo.jpg"
-        },
-        {
-            "name": "Lorem Ipsum",
-            "description": "Lorem Ipsum Lorem",
-            "imgLink": "https://images.pexels.com/photos/3244513/pexels-photo-3244513.jpeg"
-        },
-        {
-            "name": "Lorem Ipsum",
-            "description": "Lorem Ipsum Lorem",
-            "imgLink": "https://images.pexels.com/photos/3493777/pexels-photo-3493777.jpeg"
-        },
-    ]
-    const column4 = [
-        {
-            "name": "Lorem Ipsum",
-            "description": "Lorem Ipsum Lorem",
-            "imgLink": "https://images.pexels.com/photos/3493777/pexels-photo-3493777.jpeg"
-        },
-        {
-            "name": "Lorem Ipsum",
-            "description": "Lorem Ipsum Lorem",
-            "imgLink": "https://images.pexels.com/photos/4101555/pexels-photo-4101555.jpeg"
-        },
-        {
-            "name": "Lorem Ipsum",
-            "description": "Lorem Ipsum Lorem",
-            "imgLink": "https://images.pexels.com/photos/1784577/pexels-photo-1784577.jpeg"
-        },
-        {
-            "name": "Lorem Ipsum",
-            "description": "Lorem Ipsum Lorem",
-            "imgLink": "https://images.pexels.com/photos/3493777/pexels-photo-3493777.jpeg"
-        },
-        {
-            "name": "Lorem Ipsum",
-            "description": "Lorem Ipsum Lorem",
-            "imgLink": "https://images.pexels.com/photos/2113566/pexels-photo-2113566.jpeg"
-        },
-        {
-            "name": "Lorem Ipsum",
-            "description": "Lorem Ipsum Lorem",
-            "imgLink": "https://images.pexels.com/photos/1770809/pexels-photo-1770809.jpeg"
-        },
-        {
-            "name": "Lorem Ipsum",
-            "description": "Lorem Ipsum Lorem",
-            "imgLink": "https://images.pexels.com/photos/15286/pexels-photo.jpg"
-        },
-        {
-            "name": "Lorem Ipsum",
-            "description": "Lorem Ipsum Lorem",
-            "imgLink": "https://images.pexels.com/photos/3244513/pexels-photo-3244513.jpeg"
-        },
-    ]
-    
-    // var a;
-    var a = "https://images.pexels.com/photos/2113566/pexels-photo-2113566.jpeg";
-    const [show, setShow] = useState(false)
-
-    function set(p){
-        a = p;
-        // setShow(true);
-    }
-
-
     return (
         <div id='canvas'>
             <h1 className='text-center text-5xl p-10 text-[#fff]'>Events Gallery</h1>
-            <div id='row'>
-                {/* <div id='row' className='flex flex-wrap py-0 px-6'> */}
-                <div className='column'>
-                    {column1.map((details) => (
-                        <div>
-                            <img onClick={()=>{
-                                {set(details.imgLink)};
-                                setShow(true);
-                            }} src={details.imgLink} alt={details.name} className='myImg' />
-                        </div>
-
-                    ))}
-                </div>
-                <div className='column'>
-                    {column2.map((details) => (
-                        <div>
-                            <img onClick={()=>{
-                                {set(details.imgLink)};
-                                setShow(true);
-                            }} src={details.imgLink} alt={details.name} className='myImg' />
-                        </div>
-                    ))}
-                </div>
-                <div className='column'>
-                    {column3.map((details) => (
-                        <div>
-                            <img onClick={()=>{
-                                {set(details.imgLink)};
-                                setShow(true);
-                            }} src={details.imgLink} alt={details.name} className='myImg' />
-                        </div>
-                    ))}
-                </div>
-                <div className='column'>
-                    {column4.map((details) => (
-                        <div>
-                            <img onClick={()=>{
-                                {set(details.imgLink)};
-                                setShow(true);
-                            }} src={details.imgLink} alt={details.name} className='myImg' />
-                        </div>
-                    ))}
-                </div>
-
+            <div id="navigation" className='flex flex-wrap gap-20  justify-center mt-3'>
+                <NavLink className='album_name text-center' to='./album1'>Album-1</NavLink>
+                <NavLink className='album_name text-center' to='./album2'>Album-2</NavLink>
+                <NavLink className='album_name text-center' to='./album3'>Album-3</NavLink>
             </div>
 
-            <Modal onClose={() => setShow(false)} show={show}>
-                <img src={a} />
-            </Modal>          
+            <Routes>
+                <Route exact path="/" element={<Album1 />} />
+                <Route exact path="/album1" element={<Album1 />} />
+                <Route exact path="/album2" element={<Album2 />} />
+                <Route exact path="/album3" element={<Album3 />} />
+            </Routes>
+
         </div>
 
     )
 }
+
 
 export default Gallery
