f5968c2efc6f645d982982483bbb29aaf9cfa2ef
AtriSukul1508 <95478617+AtriSukul1508@users.noreply.github.com>
2023-02-11 18:52:40 +0530

    create blog responsiveness

--- src/components/BlogPage/CreateBlog.jsx
+++ src/components/BlogPage/CreateBlog.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import './blog.css'
 
 const CreateBlog = () => {
     return (
@@ -8,17 +8,17 @@ const CreateBlog = () => {
                 <div>
                     <h1 className='text-5xl p-6 text-[#295C7A]'>Create Your Blog</h1>
                 </div>
-                <div className='flex shadow-[1px_3px_7px_#4446] rounded p-4 w-[80vw] bg-[#EEEEEE] gap-4'>
+                <div className='create-blog-container flex shadow-[1px_3px_7px_#4446] rounded p-4 w-[80vw] bg-[#EEEEEE] gap-4 md:flex sm:flex flex-col'>
                     <div className='flex flex-col items-center justify-center gap-4'>
                         <div>
-                            <img src='https://images.unsplash.com/photo-1610823796603-59669e8ee29c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80' alt='Blog_image' className='rounded h-[60vh] w-[70vw]'  />
+                            <img src='https://images.unsplash.com/photo-1610823796603-59669e8ee29c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80' alt='Blog_image' className='rounded h-[60vh] w-[70vw]' />
                         </div>
                         <div>
                             <input type="file" id="img" className='hidden' accept="application/jpg" />
                             <label for='img' className='flex flex-col font-semibold border border-[#295C7A] justify-center items-center py-2 px-10 text-[#295C7A] w-full' >Choose photo</label>
                         </div>
                     </div>
-                    <div className='flex flex-col gap-4'>
+                    <form className='create-blog-form flex flex-col gap-4'>
                         <div>
                             <input type='text' placeholder='Title of blog...' className='rounded px-4 py-2 shadow-[inset_-1px_-1px_4px_#4446] border-none text-[#295C7A] w-[50vw] placeholder:text-[#295C7A] focus:outline-none' />
                         </div>
@@ -33,7 +33,7 @@ const CreateBlog = () => {
                             <button className='border font-semibold border-[#295C7A] rounded justify-center items-center py-2 px-10 text-[#295C7A] w-full'>SAVE</button>
                             <button className='border font-semibold border-[#295C7A] rounded justify-center items-center py-2 px-10 text-[#295C7A] w-full' >CLEAR</button>
                         </div>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
