761a53479926a248c742d7fb2b361acb1df8e705
Wasim Reja <iwasimreja@gmail.com>
2023-02-13 19:19:43 +0530

    fix: events gallery ui

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -3,7 +3,6 @@ import { Routes, NavLink, Route } from "react-router-dom";
 import Album1 from './Album1';
 import Album2 from './Album2';
 import Album3 from './Album3';
-// import Modal from './Modal';
 import './style.css';
 
 
@@ -12,9 +11,9 @@ const Gallery = () => {
         <div id='canvas'>
             <h1 className='text-center text-5xl p-10 text-[#fff]'>Events Gallery</h1>
             <div id="navigation" className='flex flex-wrap gap-20  justify-center mt-3'>
-                <NavLink className='album_name text-center' to='./album1'>Album-1</NavLink>
-                <NavLink className='album_name text-center' to='./album2'>Album-2</NavLink>
-                <NavLink className='album_name text-center' to='./album3'>Album-3</NavLink>
+                <NavLink className='album_name text-center' to='./album1'>Album - 1</NavLink>
+                <NavLink className='album_name text-center' to='./album2'>Album - 2</NavLink>
+                <NavLink className='album_name text-center' to='./album3'>Album - 3</NavLink>
             </div>
 
             <Routes>
