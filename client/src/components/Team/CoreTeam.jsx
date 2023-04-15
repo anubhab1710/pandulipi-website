ac74f96b6176fedcaee615a3c37ba05cae6b1848
Arjun Choudhury <arjunchoudhury11202@gmail.com>
2023-04-15 22:54:23 +0530

    added social links

--- client/src/components/Team/CoreTeam.jsx
+++ client/src/components/Team/CoreTeam.jsx
@@ -1,174 +1,198 @@
-import React, { useState } from 'react'
-import Card from './Card'
-
+import React, { useState } from "react";
+import Card from "./Card";
 
 const CoreTeam = () => {
-    const [show, setShow] = useState(false)
-    const coreTeamDetails = [
-        {
-            "name": "SHREYAN GHATAK",
-            "designation": "Secretary & English Editor",
-            "department": "Civil Engineering",
-            "imgLink": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWeZj1SB4KzSbjx_oKlZpyrOLlEL9K9DW0JA&usqp=CAU",
-            "facebook": "",
-            "instagram": "",
-            "linkedin": "",
-            "github": ""
-        },
-        {
-            "name": "ANSHIK CHAND",
-            "designation": "Cashier &  Hindi Editor",
-            "department": "Civil Engineering",
-            "imgLink": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWeZj1SB4KzSbjx_oKlZpyrOLlEL9K9DW0JA&usqp=CAU",
-            "facebook": "",
-            "instagram": "",
-            "linkedin": "",
-            "github": ""
-        }, {
-            "name": "SHANKHALIKA MALLICK",
-            "designation": "Cashier & English Editor",
-            "department": "Electronics & Communication Engineering",
-            "imgLink": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWeZj1SB4KzSbjx_oKlZpyrOLlEL9K9DW0JA&usqp=CAU",
-            "facebook": "",
-            "instagram": "",
-            "linkedin": "",
-            "github": ""
-        }, {
-            "name": "ANIRBAN DEBNATH",
-            "designation": "Media & Technical Head",
-            "department": "Electronics & Communication Engineering",
-            "imgLink": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWeZj1SB4KzSbjx_oKlZpyrOLlEL9K9DW0JA&usqp=CAU",
-            "facebook": "",
-            "instagram": "",
-            "linkedin": "",
-            "github": ""
-        }, {
-            "name": "ANUBHAV SARKAR",
-            "designation": "English Editor",
-            "department": "Electronics & Communication Engineering",
-            "imgLink": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWeZj1SB4KzSbjx_oKlZpyrOLlEL9K9DW0JA&usqp=CAU",
-            "facebook": "",
-            "instagram": "",
-            "linkedin": "",
-            "github": ""
-        }, {
-            "name": "PRAKRITISH MUKHOPADHYAY",
-            "designation": "Bengali Editor",
-            "department": "Civil Engineering",
-            "imgLink": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWeZj1SB4KzSbjx_oKlZpyrOLlEL9K9DW0JA&usqp=CAU",
-            "facebook": "",
-            "instagram": "",
-            "linkedin": "",
-            "github": ""
-        }, {
-            "name": "RAJDEEP NATH",
-            "designation": "Bengali Editor",
-            "department": "Mechanical Engineering",
-            "imgLink": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWeZj1SB4KzSbjx_oKlZpyrOLlEL9K9DW0JA&usqp=CAU",
-            "facebook": "",
-            "instagram": "",
-            "linkedin": "",
-            "github": ""
-        }, {
-            "name": "BABAI GHOSH",
-            "designation": "Bengali Editor",
-            "department": "Electrical Engineering",
-            "imgLink": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWeZj1SB4KzSbjx_oKlZpyrOLlEL9K9DW0JA&usqp=CAU",
-            "facebook": "",
-            "instagram": "",
-            "linkedin": "",
-            "github": ""
-        }, {
-            "name": "DEBOPRIYA DAS",
-            "designation": "Artwork Head",
-            "department": "Civil Engineering",
-            "imgLink": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWeZj1SB4KzSbjx_oKlZpyrOLlEL9K9DW0JA&usqp=CAU",
-            "facebook": "",
-            "instagram": "",
-            "linkedin": "",
-            "github": ""
-        }, {
-            "name": "ANILABHA DATTA",
-            "designation": "Photography Head",
-            "department": "Computer Science & Engineering",
-            "imgLink": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWeZj1SB4KzSbjx_oKlZpyrOLlEL9K9DW0JA&usqp=CAU",
-            "facebook": "",
-            "instagram": "",
-            "linkedin": "",
-            "github": ""
-        }, {
-            "name": "SAYANGDIPTA SEN",
-            "designation": "Design Head",
-            "department": "Civil Engineering",
-            "imgLink": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWeZj1SB4KzSbjx_oKlZpyrOLlEL9K9DW0JA&usqp=CAU",
-            "facebook": "",
-            "instagram": "",
-            "linkedin": "",
-            "github": ""
-        }, {
-            "name": "ARNAB PARAMANIK",
-            "designation": "Design Head",
-            "department": "Mechanical Engineering",
-            "imgLink": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWeZj1SB4KzSbjx_oKlZpyrOLlEL9K9DW0JA&usqp=CAU",
-            "facebook": "",
-            "instagram": "",
-            "linkedin": "",
-            "github": ""
-        }, {
-            "name": "MAINAK MAITY",
-            "designation": "Design Head",
-            "department": "Mechanical Engineering",
-            "imgLink": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWeZj1SB4KzSbjx_oKlZpyrOLlEL9K9DW0JA&usqp=CAU",
-            "facebook": "",
-            "instagram": "",
-            "linkedin": "",
-            "github": ""
-        },
+  const [show, setShow] = useState(false);
+  const coreTeamDetails = [
+    {
+      name: "SHREYAN GHATAK",
+      designation: "Secretary & English Editor",
+      department: "Civil Engineering",
+      imgLink:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWeZj1SB4KzSbjx_oKlZpyrOLlEL9K9DW0JA&usqp=CAU",
 
-    ]
+      linkedin: "",
+      github: "",
+    },
+    {
+      name: "ANSHIK CHAND",
+      designation: "Cashier &  Hindi Editor",
+      department: "Civil Engineering",
+      imgLink:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWeZj1SB4KzSbjx_oKlZpyrOLlEL9K9DW0JA&usqp=CAU",
 
+      linkedin: "https://www.linkedin.com/in/anshik-chand-2b32381b4",
+      github: "",
+    },
+    {
+      name: "SHANKHALIKA MALLICK",
+      designation: "Cashier & English Editor",
+      department: "Electronics & Communication Engineering",
+      imgLink:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWeZj1SB4KzSbjx_oKlZpyrOLlEL9K9DW0JA&usqp=CAU",
 
-    return (
-        <div>
-            <h1 className='text-center text-5xl p-10 text-[#295C7A]'>Meet the Core Team</h1>
-            <div>
-                <div className='flex justify-center items-center flex-wrap gap-x-32 gap-y-8 mb-10 px-10'>
-                    {coreTeamDetails.map((details) => (
-                        <div className='flex flex-col items-center gap-4'>
-                            <div className='relative'>
-                                <img src={details.imgLink} alt={details.name} className='rounded-full mx-auto  w-48 h-48' />
-                                <button onClick={() => { setShow(true) }} className='absolute bottom-4 right-[-3px] rounded-full py-[6px] px-[15px] bg-transparent '>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill='#fff' viewBox="0 0 20 20" className='white shadow-md rounded-full'>
-                                        <path d="M10,20A10,10,0,1,0,0,10,10,10,0,0,0,10,20ZM4.049,9h8.559L10.342,6.735l1.414-1.414,4.735,4.733-4.735,4.734-1.414-1.414L12.718,11H4.049Z" />
-                                    </svg>
-                                </button>
-                            </div>
-                            <div>
-                                <p className='text-center font-bold'>{details.name}</p>
-                                <p className='text-center'>{details.designation}</p>
-                            </div>
-                        </div>
-                    ))}
-                </div>
+      linkedin: "https://in.linkedin.com/in/shankhalika-mallick-248b3819a",
+      github: "",
+    },
+    {
+      name: "ANIRBAN DEBNATH",
+      designation: "Media & Technical Head",
+      department: "Electronics & Communication Engineering",
+      imgLink:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWeZj1SB4KzSbjx_oKlZpyrOLlEL9K9DW0JA&usqp=CAU",
 
+      linkedin: "https://www.linkedin.com/in/anirban-debnath-9977011a2",
+      github: "",
+    },
+    {
+      name: "ANUBHAV SARKAR",
+      designation: "English Editor",
+      department: "Electronics & Communication Engineering",
+      imgLink:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWeZj1SB4KzSbjx_oKlZpyrOLlEL9K9DW0JA&usqp=CAU",
+
+      linkedin: "https://www.linkedin.com/in/anubhabsarkar/",
+      github: "",
+    },
+    {
+      name: "PRAKRITISH MUKHOPADHYAY",
+      designation: "Bengali Editor",
+      department: "Civil Engineering",
+      imgLink:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWeZj1SB4KzSbjx_oKlZpyrOLlEL9K9DW0JA&usqp=CAU",
+
+      linkedin: "https://www.linkedin.com/mwlite/in/pmuk",
+      github: "",
+    },
+    {
+      name: "RAJDEEP NATH",
+      designation: "Bengali Editor",
+      department: "Mechanical Engineering",
+      imgLink:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWeZj1SB4KzSbjx_oKlZpyrOLlEL9K9DW0JA&usqp=CAU",
+
+      linkedin: "https://www.linkedin.com/in/rajdeep-nath-4b0877198/",
+      github: "",
+    },
+    {
+      name: "BABAI GHOSH",
+      designation: "Bengali Editor",
+      department: "Electrical Engineering",
+      imgLink:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWeZj1SB4KzSbjx_oKlZpyrOLlEL9K9DW0JA&usqp=CAU",
+
+      linkedin:
+        "https://www.linkedin.com/in/babai-ghosh-9930a1211?trk=contact-info",
+      github: "",
+    },
+    {
+      name: "DEBOPRIYA DAS",
+      designation: "Artwork Head",
+      department: "Civil Engineering",
+      imgLink:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWeZj1SB4KzSbjx_oKlZpyrOLlEL9K9DW0JA&usqp=CAU",
+
+      linkedin: "https://www.linkedin.com/in/debopriya-das-8502a41b4",
+      github: "",
+    },
+    {
+      name: "ANILABHA DATTA",
+      designation: "Photography Head",
+      department: "Computer Science & Engineering",
+      imgLink:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWeZj1SB4KzSbjx_oKlZpyrOLlEL9K9DW0JA&usqp=CAU",
+
+      linkedin: "https://www.linkedin.com/in/anilabha-datta",
+      github: "",
+    },
+    {
+      name: "SAYANGDIPTA SEN",
+      designation: "Design Head",
+      department: "Civil Engineering",
+      imgLink:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWeZj1SB4KzSbjx_oKlZpyrOLlEL9K9DW0JA&usqp=CAU",
+
+      linkedin: "http://www.linkedin.com/in/sayangdiptasen1006",
+      github: "",
+    },
+    {
+      name: "ARNAB PARAMANIK",
+      designation: "Design Head",
+      department: "Mechanical Engineering",
+      imgLink:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWeZj1SB4KzSbjx_oKlZpyrOLlEL9K9DW0JA&usqp=CAU",
+
+      linkedin: "https://www.linkedin.com/in/arnab-paramanik-1b8286174/",
+      github: "",
+    },
+    {
+      name: "MAINAK MAITY",
+      designation: "Design Head",
+      department: "Mechanical Engineering",
+      imgLink:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWeZj1SB4KzSbjx_oKlZpyrOLlEL9K9DW0JA&usqp=CAU",
+
+      linkedin: "https://www.linkedin.com/in/mainak-maiti-56657b192",
+      github: "",
+    },
+  ];
+
+  return (
+    <div>
+      <h1 className="text-center text-5xl p-10 text-[#295C7A]">
+        Meet the Core Team
+      </h1>
+      <div>
+        <div className="flex justify-center items-center flex-wrap gap-x-32 gap-y-8 mb-10 px-10">
+          {coreTeamDetails.map((details) => (
+            <div className="flex flex-col items-center gap-4">
+              <div className="relative">
+                <img
+                  src={details.imgLink}
+                  alt={details.name}
+                  className="rounded-full mx-auto  w-48 h-48"
+                />
+                <button
+                  onClick={() => {
+                    setShow(true);
+                  }}
+                  className="absolute bottom-4 right-[-3px] rounded-full py-[6px] px-[15px] bg-transparent "
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="35"
+                    height="35"
+                    fill="#fff"
+                    viewBox="0 0 20 20"
+                    className="white shadow-md rounded-full"
+                  >
+                    <path d="M10,20A10,10,0,1,0,0,10,10,10,0,0,0,10,20ZM4.049,9h8.559L10.342,6.735l1.414-1.414,4.735,4.733-4.735,4.734-1.414-1.414L12.718,11H4.049Z" />
+                  </svg>
+                </button>
+              </div>
+              <div>
+                <p className="text-center font-bold">{details.name}</p>
+                <p className="text-center">{details.designation}</p>
+              </div>
             </div>
-            <Card onClose={() => setShow(false)} show={show} />
-
-
+          ))}
         </div>
-    )
-}
-
-Card.defaultProps =
-{
-    "name": "Lorem Ipsum",
-    "designation": "Lorem Ipsum Lorem",
-    "department": "Lorem Ipsum",
-    "imgLink": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWeZj1SB4KzSbjx_oKlZpyrOLlEL9K9DW0JA&usqp=CAU",
-    "facebook": "",
-    "instagram": "",
-    "linkedin": "",
-    "github": ""
+      </div>
+      <Card onClose={() => setShow(false)} show={show} />
+    </div>
+  );
 };
 
-export default CoreTeam
+Card.defaultProps = {
+  name: "Lorem Ipsum",
+  designation: "Lorem Ipsum Lorem",
+  department: "Lorem Ipsum",
+  imgLink:
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWeZj1SB4KzSbjx_oKlZpyrOLlEL9K9DW0JA&usqp=CAU",
+  facebook: "",
+  instagram: "",
+  linkedin: "",
+  github: "",
+};
+
+export default CoreTeam;
--- client/src/components/Team/WebTeam.jsx
+++ client/src/components/Team/WebTeam.jsx
@@ -1,96 +1,113 @@
-import React, { useState } from 'react'
-import Card from './Card'
+import React, { useState } from "react";
+import Card from "./Card";
+
+const webTeamDetails = [
+  {
+    name: "WASIM REJA",
+    designation: "Frontend Lead",
+    department: "Information Technology",
+    imgLink:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWeZj1SB4KzSbjx_oKlZpyrOLlEL9K9DW0JA&usqp=CAU",
+    linkedin: "https://www.linkedin.com/in/wasimreja/",
+    github: "https://github.com/wasimreja/",
+  },
+  {
+    name: "SAMRAT SADHU",
+    designation: "Backend Lead",
+    department: "Electronics & Communication Engineering",
+    imgLink:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWeZj1SB4KzSbjx_oKlZpyrOLlEL9K9DW0JA&usqp=CAU",
+    linkedin: "https://www.linkedin.com/in/samrat-sadhu-744874202",
+    github: "https://github.com/Samrat-14",
+  },
+  {
+    name: "ARJUN CHOUDHURY",
+    designation: "Frontend Developer",
+    department: "Information Technology",
+    imgLink:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWeZj1SB4KzSbjx_oKlZpyrOLlEL9K9DW0JA&usqp=CAU",
+    linkedin: "https://www.linkedin.com/in/arjun-choudhury-469671227/",
+    github: "https://github.com/arjunchoudhury07",
+  },
+  {
+    name: "ATRI SUKUL",
+    designation: "Backend Developer",
+    department: "Computer Science & Engineering",
+    imgLink:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWeZj1SB4KzSbjx_oKlZpyrOLlEL9K9DW0JA&usqp=CAU",
+    linkedin: "https://www.linkedin.com/in/atri-sukul-35a9b022a/",
+    github: "https://github.com/AtriSukul1508",
+  },
+  {
+    name: "SAYAN KUMAR BHOWMICK",
+    designation: "Frontend Developer",
+    department: "Computer Science & Engineering",
+    imgLink:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWeZj1SB4KzSbjx_oKlZpyrOLlEL9K9DW0JA&usqp=CAU",
+    linkedin: "https://www.linkedin.com/in/sayankr007",
+    github: "https://github.com/Sayankumar007",
+  },
+];
 
 const WebTeam = () => {
-    const [show, setShow] = useState(false)
-    const webTeamDetails = [
-        {
-            "name": "WASIM REJA",
-            "designation": "Frontend Lead",
-            "department": "Information Technology",
-            "imgLink": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWeZj1SB4KzSbjx_oKlZpyrOLlEL9K9DW0JA&usqp=CAU",
-            "facebook": "",
-            "instagram": "",
-            "linkedin": "",
-            "github": ""
-        }, {
-            "name": "SAMRAT SADHU",
-            "designation": "Backend Lead",
-            "department": "Electronics & Communication Engineering",
-            "imgLink": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWeZj1SB4KzSbjx_oKlZpyrOLlEL9K9DW0JA&usqp=CAU",
-            "facebook": "",
-            "instagram": "",
-            "linkedin": "",
-            "github": ""
-        }, {
-            "name": "ARJUN CHOUDHURY",
-            "designation": "Frontend Developer",
-            "department": "Information Technology",
-            "imgLink": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWeZj1SB4KzSbjx_oKlZpyrOLlEL9K9DW0JA&usqp=CAU",
-            "facebook": "",
-            "instagram": "",
-            "linkedin": "",
-            "github": ""
-        }, {
-            "name": "ATRI SUKUL",
-            "designation": "Backend Developer",
-            "department": "Computer Science & Engineering",
-            "imgLink": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWeZj1SB4KzSbjx_oKlZpyrOLlEL9K9DW0JA&usqp=CAU",
-            "facebook": "",
-            "instagram": "",
-            "linkedin": "",
-            "github": ""
-        }, {
-            "name": "SAYAN KUMAR BHOWMICK",
-            "designation": "Frontend Developer",
-            "department": "Computer Science & Engineering",
-            "imgLink": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWeZj1SB4KzSbjx_oKlZpyrOLlEL9K9DW0JA&usqp=CAU",
-            "facebook": "",
-            "instagram": "",
-            "linkedin": "",
-            "github": ""
-        },
-    ]
+  const [show, setShow] = useState(false);
 
-    return (
-        <div className='bg-[#252b42]'>
-            <h1 className='text-center text-5xl p-10 text-[#fff]'>Meet the Web Team</h1>
-            <div>
-                <div className='flex justify-center items-center flex-wrap gap-x-32 gap-y-8 px-10 pb-10 mt-4'>
-                    {webTeamDetails.map((details) => (
-                        <div className='flex flex-col items-center gap-4'>
-                            <div className='relative'>
-                                <img src={details.imgLink} alt={details.name} className='rounded-full mx-auto  w-48 h-48' />
-                                <button onClick={() => { setShow(true) }} className='absolute bottom-4 right-[-3px] rounded-full py-[6px] px-[15px] bg-transparent'>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill='#fff' viewBox="0 0 20 20" className='white shadow-md rounded-full'>
-                                        <path d="M10,20A10,10,0,1,0,0,10,10,10,0,0,0,10,20ZM4.049,9h8.559L10.342,6.735l1.414-1.414,4.735,4.733-4.735,4.734-1.414-1.414L12.718,11H4.049Z" />
-                                    </svg>
-                                </button>
-                            </div>
-                            <div>
-                                <p className='text-center font-bold text-[#fff]'>{details.name}</p>
-                                <p className='text-center text-[#fff]'>{details.designation}</p>
-                            </div>
-                        </div>
-                    ))}
-                </div>
+  return (
+    <div className="bg-[#252b42]">
+      <h1 className="text-center text-5xl p-10 text-[#fff]">
+        Meet the Web Team
+      </h1>
+      <div>
+        <div className="flex justify-center items-center flex-wrap gap-x-32 gap-y-8 px-10 pb-10 mt-4">
+          {webTeamDetails.map((details) => (
+            <div className="flex flex-col items-center gap-4">
+              <div className="relative">
+                <img
+                  src={details.imgLink}
+                  alt={details.name}
+                  className="rounded-full mx-auto  w-48 h-48"
+                />
+                <button
+                  onClick={() => {
+                    setShow(true);
+                  }}
+                  className="absolute bottom-4 right-[-3px] rounded-full py-[6px] px-[15px] bg-transparent"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="35"
+                    height="35"
+                    fill="#fff"
+                    viewBox="0 0 20 20"
+                    className="white shadow-md rounded-full"
+                  >
+                    <path d="M10,20A10,10,0,1,0,0,10,10,10,0,0,0,10,20ZM4.049,9h8.559L10.342,6.735l1.414-1.414,4.735,4.733-4.735,4.734-1.414-1.414L12.718,11H4.049Z" />
+                  </svg>
+                </button>
+              </div>
+              <div>
+                <p className="text-center font-bold text-[#fff]">
+                  {details.name}
+                </p>
+                <p className="text-center text-[#fff]">{details.designation}</p>
+              </div>
             </div>
-            <Card onClose={() => setShow(false)} show={show} />
-
+          ))}
         </div>
-    )
-}
-
-Card.defaultProps =
-{
-    "name": "Lorem Ipsum",
-    "designation": "Lorem Ipsum Lorem",
-    "department": "Lorem Ipsum",
-    "imgLink": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWeZj1SB4KzSbjx_oKlZpyrOLlEL9K9DW0JA&usqp=CAU",
-    "facebook": "",
-    "instagram": "",
-    "linkedin": "",
-    "github": ""
+      </div>
+      <Card onClose={() => setShow(false)} show={show} />
+    </div>
+  );
 };
 
-export default WebTeam
+Card.defaultProps = {
+  name: "Lorem Ipsum",
+  designation: "Lorem Ipsum Lorem",
+  department: "Lorem Ipsum",
+  imgLink:
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWeZj1SB4KzSbjx_oKlZpyrOLlEL9K9DW0JA&usqp=CAU",
+  linkedin: "",
+  github: "",
+};
+
+export default WebTeam;
