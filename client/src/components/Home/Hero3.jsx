ac0e7383d23f22c5fdbacde1496267a5d8d91bd7
Wasim Reja <iwasimreja@gmail.com>
2023-04-18 17:05:57 +0530

    Minor change

--- client/src/components/Home/Hero3.jsx
+++ client/src/components/Home/Hero3.jsx
@@ -5,32 +5,27 @@ function Hero3() {
     const items = [
         {
             title: "Xpressions",
-            content:"The annual Creative Writing Contest is held in October. Students participate in this month-long contest and submit articles under various themes. The editorial team receives a staggering number of submissions in English, Bengali, Hindi and Nepali. From poetry to short stories, humorous incidents to argumentative essays, each write-up is filled to the brim with love.",
-            imgLink: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.tI-fwrZIwEN4vkxcU24g7AHaEK%26pid%3DApi&f=1&ipt=ce4454730c936a1c648871d4966b90dc5570c820b0fa836f85bf15dc83cfd510&ipo=images"
+            content:"The annual Creative Writing Contest is held in October. Students participate in this month-long contest and submit articles under various themes. The editorial team receives a staggering number of submissions in English, Bengali, Hindi and Nepali. From poetry to short stories, humorous incidents to argumentative essays, each write-up is filled to the brim with love."
 
         },
         {
             title: "Annual Award Ceremony",
-            content:"The Annual Award Ceremony is the most awaited event of the year. In these event smiling faces collect shiny trophies and certificates. From Budspeak to Xpressions, all the students who had achieved a position in the contests arranged by the Magazine Club are rewarded with exciting goodies and surprises for their wins.",
-            imgLink: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.tI-fwrZIwEN4vkxcU24g7AHaEK%26pid%3DApi&f=1&ipt=ce4454730c936a1c648871d4966b90dc5570c820b0fa836f85bf15dc83cfd510&ipo=images"
+            content:"The Annual Award Ceremony is the most awaited event of the year. In these event smiling faces collect shiny trophies and certificates. From Budspeak to Xpressions, all the students who had achieved a position in the contests arranged by the Magazine Club are rewarded with exciting goodies and surprises for their wins."
 
         },
         {
             title: "Tape a Tale: Stellar",
-            content:"This is usually held in March. Conducted by Tape a Tale in association with Pandulipi, the event encourages college and university students to come out and share their stories. In the year 2023 Steller’s selection round at JGEC saw judges, like Deon Demamount and Kopal Khanna, the founder of Tape a Tale herself! Jolites from all years share their experiences like dealing with trauma, life lessons, first love, heartbreaks, and humorous accounts that have happened in their college life.",
-            imgLink: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.tI-fwrZIwEN4vkxcU24g7AHaEK%26pid%3DApi&f=1&ipt=ce4454730c936a1c648871d4966b90dc5570c820b0fa836f85bf15dc83cfd510&ipo=images"
+            content:"This is usually held in March. Conducted by Tape a Tale in association with Pandulipi, the event encourages college and university students to come out and share their stories. In the year 2023 Steller’s selection round at JGEC saw judges, like Deon Demamount and Kopal Khanna, the founder of Tape a Tale herself! Jolites from all years share their experiences like dealing with trauma, life lessons, first love, heartbreaks, and humorous accounts that have happened in their college life."
 
         },
         {
             title: "International Mother Language Day",
-            content:"Pandulipi celebrates the International Mother Language Day on 21st every year by inviting students across all cultures to share a write-up in their mother tongue. There are several submissions in languages from Bengali, Hindi and, Nepali, and Santhali, and it is a delight to read every writeup, and indulge in the cultural palette every language has to offer.",
-            imgLink: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.tI-fwrZIwEN4vkxcU24g7AHaEK%26pid%3DApi&f=1&ipt=ce4454730c936a1c648871d4966b90dc5570c820b0fa836f85bf15dc83cfd510&ipo=images"
+            content:"Pandulipi celebrates the International Mother Language Day on 21st every year by inviting students across all cultures to share a write-up in their mother tongue. There are several submissions in languages from Bengali, Hindi and, Nepali, and Santhali, and it is a delight to read every writeup, and indulge in the cultural palette every language has to offer."
 
         },
         {
             title: "Wall Magazine",
-            content:"This is yet another beloved tradition of Pandulipi which involves mounting a Wall Magazine in the Administrative Building. The theme of this year’s Wall Magazine was: Unity in Diversity. Students join hands to create artworks, posters and flamboyant designs that adorns the wall.",
-            imgLink: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.tI-fwrZIwEN4vkxcU24g7AHaEK%26pid%3DApi&f=1&ipt=ce4454730c936a1c648871d4966b90dc5570c820b0fa836f85bf15dc83cfd510&ipo=images"
+            content:"This is yet another beloved tradition of Pandulipi which involves mounting a Wall Magazine in the Administrative Building. The theme of this year’s Wall Magazine was: Unity in Diversity. Students join hands to create artworks, posters and flamboyant designs that adorns the wall."
         },
     ]
 
@@ -39,7 +34,7 @@ function Hero3() {
         <h2 className='text-5xl text-center p-10 text-[#295C7A] xl:text-7xl'>LATEST EVENTS</h2>
         <div className='flex justify-start items-center overflow-x-scroll scrollbar-hide gap-x-10 p-5 pb-20'>
             {items.map((item) =>
-                <LatestEventCard title={item.title} content={item.content} imgLink={item.imgLink}/>
+                <LatestEventCard title={item.title} content={item.content}/>
             )}
         </div>
     </div>
--- client/src/components/Home/LatestEventCard.jsx
+++ client/src/components/Home/LatestEventCard.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
 
-function LatestEventCard({title, content, imgLink}) {
+function LatestEventCard({title, content}) {
   return (
     <div className="flex justify-center hover:scale-110 transition-all flex-shrink-0">
         <div className="rounded-lg bg-white max-w-xs md:max-w-md shadow-2xl">
-            <a href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">
-                <img className="rounded-t-lg w-full" src={imgLink} alt=""/>
-            </a>
             <div className="p-10 flex gap-y-5 flex-col items-center">
                 <h5 className="text-gray-900 text-3xl text-center font-medium mb-2 md:text-5xl">{title}</h5>
                 <p className="text-justify overflow-hidden h-28 md:h-56 xl:text-xl md:text-left">{content}</p>
