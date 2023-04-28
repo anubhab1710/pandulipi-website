1366bbc67029627e59942ff2f0b58a458e80b1d6
Wasim Reja <iwasimreja@gmail.com>
2023-04-29 00:52:18 +0530

    fix broken link

--- client/src/components/eLibrary/MoreMagazines.jsx
+++ client/src/components/eLibrary/MoreMagazines.jsx
@@ -14,37 +14,37 @@ const MagazineMore = () => {
       name: "Pandulipi",
       year: "2018",
       imgLink: cover_2018,
-      downloadLink: "#######",
+      downloadLink: "https://drive.google.com/file/d/1VXUfS1A4RPn4Ypzg0k0awX1z-BfZMKNq/view?usp=sharing",
     },
     {
       name: "Pandulipi",
       year: "2016",
       imgLink: cover_2016,
-      downloadLink: "#######",
+      downloadLink: "https://drive.google.com/file/d/1dHOUV7PRCDicqRDW-zdSsq5fRUYgfS9Z/view?usp=sharing",
     },
     {
       name: "Pandulipi",
       year: "2014",
       imgLink: cover_2014,
-      downloadLink: "#######",
+      downloadLink: "https://drive.google.com/file/d/1m670uaJM-PA9zUszX_gcmi8HLwF6OtJQ/view?usp=sharing",
     },
     {
       name: "Pandulipi",
       year: "1990",
       imgLink: cover_1990,
-      downloadLink: "#######",
+      downloadLink: "https://drive.google.com/file/d/1s_jgMnMuoP5V40CTxRxkVqyBNe7yBrHZ/view?usp=sharing",
     },
     {
       name: "Pandulipi",
       year: "1985",
       imgLink: cover_1985,
-      downloadLink: "#######",
+      downloadLink: "https://drive.google.com/file/d/1STzcWm5S18Q1qTLp85QVHzeEhNztuQ3L/view?usp=sharing",
     },
     {
       name: "Pandulipi",
       year: "1982",
       imgLink: cover_1982,
-      downloadLink: "#######",
+      downloadLink: "https://drive.google.com/file/d/1A3DVdFoOKmcLKOzp6Qn4mSCZge4mqDNx/view?usp=sharing",
     },
   ];
 
