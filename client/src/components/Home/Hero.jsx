88de0ef2fac9956f495a670e830396e5605dbc23
Arjun Choudhury <arjunchoudhury11202@gmail.com>
2023-04-30 22:43:01 +0530

    changed background image

--- client/src/components/Home/Hero.jsx
+++ client/src/components/Home/Hero.jsx
@@ -1,16 +1,19 @@
 import React from "react";
+import hero from "../../assets/img/hero.jpg";
 
 function Hero() {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center">
       <img
         className="absolute -z-10 h-[120vh] w-full object-cover"
-        src="https://img.freepik.com/premium-photo/simple-composition-from-many-hardback-books-tinted-classic-blue-color_96336-515.jpg?w=2000"
+        src={hero}
         alt=""
       />
       <div className="m-5 py-14 rounded-3xl max-w-7xl bg-gray-300 bg-opacity-50 backdrop-blur-md transition-all duration-500 flex flex-col justify-center items-center">
         <h1 className="text-7xl xl:text-8xl">Pandulipi</h1>
-        <h2 className="text-5xl xl:text-7xl text-center px-10 text-[#295C7A]">The Magazine Club of JGEC</h2>
+        <h2 className="text-5xl xl:text-7xl text-center px-10 text-[#295C7A]">
+          The Magazine Club of JGEC
+        </h2>
         <div className="p-10 md:p-20 text-justify max-w-6xl xl:text-2xl">
           <p>
             Soon after the Jalpaiguri Government Engineering College came into
